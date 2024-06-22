a59e1efd2f1b0f8c42545018aeace27957668915
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-06-22 22:06:09 +0400

    attempted process.env in http.js

--- utils/http.ts
+++ utils/http.ts
@@ -22,11 +22,24 @@ export interface SearchParams {
 export async function searchRestaurants(options: SearchParams = {}) {
   const { query = "*", filters = "", perPage = 20, pageIndex = 0 } = options;
 
+  // const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+  // const ALGOLIA_APPLICATION_ID = process.env.ALGOLIA_APPLICATION_ID;
+
+  // if (!ALGOLIA_API_KEY || !ALGOLIA_APPLICATION_ID) {
+  //   throw new Error(
+  //     "Algolia API key or Application ID is not set in the environment variables"
+  //   );
+  // }
+
   const requestUrl = `https://VVWOVRO2RI-dsn.algolia.net/1/indexes/restaurants?query=${query}&hitsPerPage=${perPage}&page=${pageIndex}&attributesToRetrieve=name,rating,coverImage,onlyOnDeliveroo,deliveryFee,open&attributesToHighlight=%5B%5D&filters=${filters}`;
   console.log(requestUrl);
 
   try {
     const response = await fetch(requestUrl, {
+      // headers: new Headers({
+      //   "X-Algolia-API-Key": ALGOLIA_API_KEY,
+      //   "X-Algolia-Application-Id": ALGOLIA_APPLICATION_ID,
+      // }),
       headers: new Headers({
         "X-Algolia-API-Key": "3820ce633e4c4abb9400dfb74df646be",
         "X-Algolia-Application-Id": "VVWOVRO2RI",
