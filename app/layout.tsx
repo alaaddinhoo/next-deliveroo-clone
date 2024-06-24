239ab803d01b2a50402bf387bb75d582de28b630
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-06-24 06:07:35 +0400

    last

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,21 +19,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-[100vh] flex flex-col ">
-        <div className="grow">{children}</div>
-        <div className="bg-gradient-to-r text-xl from-[#00ccbb] h-[10vh] w-full flex items-center justify-center text-white font-semibold">
-          <span>Made with&nbsp;</span>
-          <span className="inline-block">
-            <Heart className="fill-rose-400" color="rose-400" size={24} />
-          </span>
-          <span>&nbsp;by&nbsp;</span>
-          <span className="inline-block">
-            <Link
-              className="underline font-semibold"
-              href="https://codewithalaa.netlify.app/"
-            >
-              Alaaeldin
-            </Link>
-          </span>
+        <div className="md:hidden bg-primary w-full h-full">
+          Only desktop version available right now
+        </div>
+
+        <div className="hidden md:block">
+          <div className="grow">{children}</div>
+          <div className="bg-gradient-to-r text-xl from-[#00ccbb] h-[10vh] w-full flex items-center justify-center text-white font-semibold">
+            <span>Made with&nbsp;</span>
+            <span className="inline-block">
+              <Heart className="fill-rose-400" color="rose-400" size={24} />
+            </span>
+            <span>&nbsp;by&nbsp;</span>
+            <span className="inline-block">
+              <Link
+                className="underline font-semibold"
+                href="https://codewithalaa.netlify.app/"
+              >
+                Alaaeldin
+              </Link>
+            </span>
+          </div>
         </div>
       </body>
     </html>
