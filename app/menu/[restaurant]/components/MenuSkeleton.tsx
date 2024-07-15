b952024d6afc388e14a69b8734822f0b7d871faa
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-07-15 20:52:12 +0400

    Update MenuSkeleton.tsx

--- app/menu/[restaurant]/components/MenuSkeleton.tsx
+++ app/menu/[restaurant]/components/MenuSkeleton.tsx
@@ -25,40 +25,30 @@ export const MenuSkeleton = () => {
         </div>
 
         <div className="  bg-[#f9fbfa] pt-8">
-          <div className="flex gap-4 px-[4vw] md:px-[64px]">
-            <div className="flex flex-col flex-wrap gap-12 grow ">
-              {Array.from({ length: 1 }).map((_, index) => (
-                <div key={index}>
-                  <div className="text-2xl">
-                    <Skeleton />
-                  </div>
-                  <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-                    {Array.from({ length: 6 }).map((_, index2) => (
-                      <div
-                        key={index + index2}
-                        className="flex justify-between items-center  bg-white p-6"
-                      >
-                        <div className="w-[150px] space-y-2">
-                          <Skeleton className="w-full h-[25px] rounded-none" />
-                          <Skeleton className="w-full h-[25px] rounded-none" />
-                          <Skeleton className="w-full h-[25px] rounded-none" />
-                        </div>
-
-                        <Skeleton className="w-[150px] h-[150px]" />
-                      </div>
-                    ))}
-                  </div>
+          <div className="flex flex-col flex-wrap gap-12 grow px-[4vw] md:px-[64px]">
+            {Array.from({ length: 1 }).map((_, index) => (
+              <div key={index}>
+                <div className="text-2xl">
+                  <Skeleton />
                 </div>
-              ))}
-            </div>
+                <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                  {Array.from({ length: 6 }).map((_, index2) => (
+                    <div
+                      key={index + index2}
+                      className="flex justify-between items-center  bg-white p-6"
+                    >
+                      <div className="w-[150px] space-y-2">
+                        <Skeleton className="w-full h-[25px] rounded-none" />
+                        <Skeleton className="w-full h-[25px] rounded-none" />
+                        <Skeleton className="w-full h-[25px] rounded-none" />
+                      </div>
 
-            <div className="flex flex-col gap-4 self-start grow items-center p-12  bg-white shadow-sm  sticky top-[0px]">
-              <Skeleton className="w-full h-[25px] rounded-none" />
-
-              <Skeleton className="w-full h-[25px] rounded-none" />
-
-              <Skeleton className="w-full h-[25px] rounded-none" />
-            </div>
+                      <Skeleton className="w-[150px] h-[150px]" />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
