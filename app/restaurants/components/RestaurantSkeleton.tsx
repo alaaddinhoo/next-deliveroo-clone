deb03ddfa10503be9842411ae85a4f419196f1a4
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-07-15 20:13:08 +0400

    Update RestaurantSkeleton.tsx

--- app/restaurants/components/RestaurantSkeleton.tsx
+++ app/restaurants/components/RestaurantSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const RestaurantSkeleton = ({ num }: { num: number }) => {
   return (
     <div className="w-full">
-      <div className="grid grid-cols-6 gap-4 w-full justify-evenly ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 2xl:grid-cols-6 gap-4 w-full justify-evenly ">
         {Array.from({ length: num }).map((_, index) => (
           <div className="flex flex-col space-y-4 p-2">
             <Skeleton className="h-[125px] rounded-none" />
