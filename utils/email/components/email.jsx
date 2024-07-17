5b8fb4e9a58b9b0f30ca054f38eb790882787511
alaaeldin-92 <alaaeldin.92@outlook.com>
2024-07-17 10:47:04 +0400

    added white logo, updated email template

--- utils/email/components/email.jsx
+++ utils/email/components/email.jsx
@@ -1,12 +1,110 @@
 import * as React from "react";
-import { Html, Button } from "@react-email/components";
+import {
+  Body,
+  Button,
+  Container,
+  Column,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+  Tailwind,
+} from "@react-email/components";
 
 export function Email(props) {
   const { url } = props;
 
   return (
-    <Html lang="en">
-      <Button href={url}>Verify Email</Button>
+    <Html>
+      <Head />
+      {/* <Preview>{previewText}</Preview> */}
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans px-2">
+          <Container className="border border-solid border-[#eaeaea] rounded my-[40px] mx-auto max-w-[465px]">
+            <Section className="py-[32px] bg-[#00ccbb] h-32">
+              <Img
+                src="https://consumer-component-library.roocdn.com/30.2.0/static/images/logo-teal.svg"
+                width="90%"
+                height="37"
+                className="my-0 mx-auto"
+              />
+            </Section>
+            <Container className="p-[20px]">
+              <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+                Join <strong>Deliveroo</strong> on <strong>Vercel</strong>
+              </Heading>
+              <Text className="text-black text-[14px] leading-[24px]">
+                Welcome!,
+              </Text>
+              <Text className="text-black text-[14px] leading-[24px]">
+                {/* <strong>{invitedByUsername}</strong> (
+              <Link
+                href={`mailto:${invitedByEmail}`}
+                className="text-blue-600 no-underline"
+              >
+                {invitedByEmail}
+              </Link>
+              ) has invited you to the <strong>{teamName}</strong> team on{" "}
+              <strong>Vercel</strong>. */}
+              </Text>
+              {/* <Section>
+              <Row>
+                <Column align="right">
+                  <Img
+                    className="rounded-full"
+                    src={userImage}
+                    width="64"
+                    height="64"
+                  />
+                </Column>
+                <Column align="center">
+                  <Img
+                    src={`${baseUrl}/static/vercel-arrow.png`}
+                    width="12"
+                    height="9"
+                    alt="invited you to"
+                  />
+                </Column>
+                <Column align="left">
+                  <Img
+                    className="rounded-full"
+                    src={teamImage}
+                    width="64"
+                    height="64"
+                  />
+                </Column>
+              </Row>
+            </Section> */}
+              <Section className="text-center mt-[32px] mb-[32px]">
+                <Button
+                  className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
+                  href={url}
+                >
+                  Join the team
+                </Button>
+              </Section>
+              <Text className="text-black text-[14px] leading-[24px]">
+                or copy and paste this URL into your browser:{" "}
+                <Link href={url} className="text-blue-600 no-underline">
+                  {url}
+                </Link>
+              </Text>
+              <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+              <Text className="text-[#666666] text-[12px] leading-[24px]">
+                If you were not expecting this invitation, you can ignore this
+                email. If you are concerned about your account's safety, please
+                reply to this email to get in touch with us.
+              </Text>
+            </Container>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 }
